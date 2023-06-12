7ff11997b2ae303d2b9c165221128c0f5ccf7f60
rbcribeiro <rbcribeiro@gmail.com>
2023-06-12 10:18:00 -0300

    Updates

---
+++ main.js
@@ -0,0 +1,34 @@
+import login from './pages/login/login.js';
+import register from './pages/register/register.js';
+import timeline from './pages/timeline/timeline.js';
+
+const main = document.querySelector('#root');
+
+const renderPage = () => {
+  main.innerHTML = '';
+  const hash = window.location.hash;
+
+  switch (hash) {
+    case '#register':
+      main.appendChild(register());
+      break;
+    case '#timeline':
+      main.appendChild(timeline());
+      break;
+    default:
+      main.appendChild(login());
+      break;
+  }
+};
+
+window.addEventListener('hashchange', renderPage);
+
+window.addEventListener('load', () => {
+  if (window.location.hash === '#register') {
+    main.appendChild(register());
+  } else if (window.location.hash === '#timeline') {
+    main.appendChild(timeline());
+  } else {
+    main.appendChild(login());
+  }
+});
---
+++ pages/register/register.js
@@ -0,0 +1,78 @@
+import { errorsFirebase, validateRegister } from '../../validations.js';
+import { createUserWithEmail } from '../../firebase/auth.js';
+
+export default () => {
+  function getInputValues() {
+    const name = document.getElementById('name-user').value;
+    const lastName = document.getElementById('lastName-user').value;
+    const email = document.getElementById('email-user').value;
+    const password = document.getElementById('register-password').value;
+
+    return {
+      name,
+      lastName,
+      email,
+      password,
+    };
+  }
+
+  function printErrorMessage(message) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = message;
+  }
+
+  async function registerUser() {
+    const {
+      name,
+      lastName,
+      email,
+      password,
+    } = getInputValues();
+    const validationErrors = validateRegister(name, lastName, email, password);
+
+    if (validationErrors.length > 0) {
+      printErrorMessage(validationErrors);
+      return;
+    }
+
+    try {
+      await createUserWithEmail(name, email, password, lastName);
+      window.location.hash = '#timeline';
+    } catch (error) {
+      console.error('Erro ao registrar o usuário:', error);
+      const printError = errorsFirebase(error.code);
+      printErrorMessage(printError || 'Erro ao registrar o usuário');
+    }
+  }
+
+  const registrationForm = document.createElement('div');
+  registrationForm.classList.add('register-container'); //verificar
+
+  const templateRegister = ` 
+    <section class='form-register'>
+      <p class='title'>Cadastrar-se</p>
+      <form class='register-form'>
+        <div>
+          <input type='text' class='inputs-register' id='name-user' placeholder='NOME'>
+          <input type='text' class='inputs-register' id='lastName-user' placeholder='SOBRENOME'>
+          <input type='text' class='inputs-register' id='email-user' placeholder='EMAIL'>
+          <input type='password' class='inputs-register' id='register-password' placeholder='CRIAR SENHA'>
+        </div>
+        <div>
+          <button type='button' class='register-btn'>CADASTRAR</button>
+        </div> 
+        <p id='error-message' class='error-message'></p>
+      </form>
+    </section>
+    <br>
+    <nav>
+      <a class='btn-back' href='#login'><img src='img/assets/arrow.png' id='arrow-img' alt='imagem de seta' width='50px'></a>
+    </nav> 
+  `;
+
+  registrationForm.innerHTML = templateRegister;
+
+  registrationForm.querySelector('.register-btn').addEventListener('click', registerUser);
+
+  return registrationForm;
+};
